629be74d6c307caae3b4fbded1e1daf1ea8ce93a
talayruk <notejde@gmail.com>
2016-09-12 10:45:28 -0700

    add leapyearTest.cs

---
+++ Tests/leapYearTest.cs
@@ -0,0 +1,12 @@
+using Xunit;
+namespace LeapYear
+{
+  public class LeapYearTest
+  {
+    [Fact]
+    public void IsLeapYear_ForNumberDivisibleByFour_true()
+    {
+      // eventually yourtesting code will go here
+    }
+  }
+}
