6d459dd07a4414920f88512cf6832e3c2ab4b9b7
talayruk <notejde@gmail.com>
2016-09-12 12:27:25 -0700

    test LeapYear

--- Tests/leapYearTest.cs
+++ Tests/leapYearTest.cs
@@ -3,10 +3,30 @@ namespace LeapYear
 {
   public class LeapYearTest
   {
+
+
     [Fact]
-    public void IsLeapYear_ForNumberDivisibleByFour_true()
+    public void IsLeapYear_ForNumberDivisibleByFour_false()
     {
-      // eventually yourtesting code will go here
+      LeapYear testLeapYear = new LeapYear();
+      Assert.Equal(false, testLeapYear.IsLeapYear(1999));
     }
+
+
+    [Fact]
+    public void IsLeapYear_ForNumberDivisibleByOneHundred_false()
+    {
+      LeapYear testLeapYear = new LeapYear();
+      Assert.Equal(false, testLeapYear.IsLeapYear(1900));
+    }
+
+    [Fact]
+    public void IsLeapYear_ForNumberDivisibleByFourHundred_true()
+    {
+      LeapYear testLeapYear = new LeapYear();
+      Assert.Equal(true, testLeapYear.IsLeapYear(2000));
+    }
+
+
   }
 }
